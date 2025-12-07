1541fe2c4b92b00e0890fa4031bc2afdc84740c2
Wolfgang Förster <wolfoerster@gmx.de>
2025-12-07 12:46:52 +0100

    Initial code

---
+++ SmartLogViewer/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+//******************************************************************************************
+// Copyright © 2017 - 2025 Wolfgang Foerster (wolfoerster@gmx.de)
+//
+// This file is part of the SmartLogViewer project which can be found on github.com
+//
+// SmartLogViewer is free software: you can redistribute it and/or modify it under the terms 
+// of the GNU General Public License as published by the Free Software Foundation, 
+// either version 3 of the License, or (at your option) any later version.
+// 
+// SmartLogViewer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//******************************************************************************************
+
+using System.Windows;
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,            //where theme specific resource dictionaries are located
+                                                //(used if a resource is not found in the page,
+                                                // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly   //where the generic resource dictionary is located
+                                                //(used if a resource is not found in the page,
+                                                // app, or any theme specific resource dictionaries)
+)]
