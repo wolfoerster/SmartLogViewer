d8a60280f4165931dbbdfa294240d19cb53af988
Wolfgang Förster <wolfoerster@gmx.de>
2025-12-12 16:34:36 +0100

    prepare code

--- SmartLogViewer.Common/Utils.cs
+++ SmartLogViewer.Common/Utils.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿//******************************************************************************************
+// Copyright © 2017 - 2026 Wolfgang Foerster (wolfoerster@gmx.de)
+//
+// This file is part of the SmartLogViewer project which can be found on github.com
+//
+// SmartLogViewer is free software: you can redistribute it and/or modify it under the terms 
+// of the GNU General Public License as published by the Free Software Foundation, 
+// either version 3 of the License, or (at your option) any later version.
+// 
+// SmartLogViewer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//******************************************************************************************
+
+using System;
 using System.Collections;
 using System.Globalization;
 using System.IO;
--- SmartLogViewer/ViewModels/PropertyChangedNotifier.cs
+++ SmartLogViewer/ViewModels/PropertyChangedNotifier.cs
@@ -15,30 +15,20 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 //******************************************************************************************
 
-namespace SmartLogViewer.Common;
+using System.ComponentModel;
 
-public interface IByteParser
+namespace SmartLogViewer.ViewModels;
+
+public class PropertyChangedNotifier : INotifyPropertyChanged, INotifyPropertyChangedPreview
 {
-    /// <summary>
-    /// Check if there are log entries of known format.
-    /// If yes, the ByteParser can preprocess the file and store the result in a new file
-    /// which is then opened with the LogReader. This step is optional.
-    /// </summary>
-    bool CheckFormat(byte[] bytes, out string? newFileName);
+    public event PropertyChangedEventHandler? PropertyChanged;
 
-    /// <summary>
-    /// The bytes which contain the log entries.
-    /// </summary>
-    byte[] Bytes { get; set; }
+    public event PropertyChangedEventHandler? PropertyChangedPreview;
 
-    /// <summary>
-    /// Gets the current position, i.e. the position of the next log entry.
-    /// </summary>
-    int CurrentPosition { get; }
+    //protected bool Checkset()
+}
 
-    /// <summary>
-    /// Reads the next log entry and updates the current position.
-    /// Returns an error string or null if no error ocurred.
-    /// </summary>
-    string? ReadNextEntry(out LogRecord logRecord);
+public interface INotifyPropertyChangedPreview
+{
+    public event PropertyChangedEventHandler? PropertyChangedPreview;
 }
